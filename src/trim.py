fdffaa831b2e2ab175a62ad099af4ea91f17f495
Brian Silvey <blujay270@gmail.com>
2017-11-04 02:15:57 -0400

    added docopt to trim

--- src/trim.py
+++ src/trim.py
@@ -1,3 +1,12 @@
+"""
+Take in a list of words from INPUT_FILE and write a list of the word stems the OUTPUT_FILE.
+Usage:
+    trim.py INPUT_FILE OUTPUT_FILE
+"""
+
 from stemming.porter2 import stem
+from docopt import docopt
 
-
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    print(args)
