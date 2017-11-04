e22b6c169ab154849c62f65d1e5bcd31ea35874d
Yu Hu <hu28y.mtholyoke.edu>
2017-11-04 02:11:31 -0400

    trim the frequent vocab list to the base form

--- src/trim.py
+++ src/trim.py
@@ -1,3 +1,16 @@
 from stemming.porter2 import stem
 
+def _parse_trim(PATH_FILE, DIR_FILE):
+    lib = []
+    for line in open(PATH_FILE):
+        line = line.rstrip('\n')
+        lib.append(stem(line))
+
+    f = open(DIR_FILE, 'w')
+    for i in range(len(lib)):
+        f.write(lib[i] + '\n')
+
+    f.close()
+
+_parse_trim("../data/google-10000-english-usa.txt", "../data/trim.txt")
 
