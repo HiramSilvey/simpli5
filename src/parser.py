8453989c16ea634b2c0562b92262c3bb6a9b591a
Brian Silvey <blujay270@gmail.com>
2017-11-04 15:39:54 -0400

    working on getting best synonym

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,11 @@
+import pickle
 import spacy
-
+from stemming.porter2 import stem
 nlp = spacy.load('en')
+
+#Map POS (spaCy form) to api
+#PUNCT, PART, SYM, X, INTJ are insignificant pos
+POS_MAP = {'PUNCT': -1, 'PART': -1, 'SYM': -1, 'X': -1, 'INTJ': -1, 'ADJ': '(adj)', 'ADV': '(adv)', 'NOUN': '(noun)'}
 
 class token:
     def __init__(self, word, pos, lemma_):
@@ -42,3 +47,31 @@ def parse(sentence):
             tokens.append(t)
             idx+=1
     return tokens
+
+def get_best_synonym(word_token):
+    '''get the returned list of synonyms from a given response'''
+    try:
+        pos = POS_MAP[word_token.get_pos()]
+        json_resp = json.loads(request(word_token.get_word()))['response']
+    except:
+        return word_token.get_word()
+    synonyms = []
+    for w_type in json_resp:
+        if w_type['list']['category'] == pos:
+            synonyms.extend(w_type['list']['synonyms'].split('|'))
+    most_freq = ('', 0)
+    tenK = pickle.load(open('../data/tenK.words', 'rb'))
+    print(synonyms)
+    for synonym in synonyms:
+        score = 0
+        for syn_token in synonym.split():
+            syn = stem(syn_token)
+            if tenK.get(syn) != None:
+                score += tenK[syn]
+            else:
+                score = 0
+                break
+        score = score/float(len(synonym))
+        if score > most_freq[1]:
+            most_freq = (synonym, score)
+    return most_freq[0]
